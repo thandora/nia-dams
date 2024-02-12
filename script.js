66c155feddf3d5da3b54b1d7f6d804626bbc87bc
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2024-02-12 16:03:13 +0800

    add on hover effect

--- script.js
+++ script.js
@@ -1,22 +1,81 @@
 import { geoData } from "./loadData.js";
 
-const coordsDolina = [11.193004, 125.003758];
+const coordsDefault = [10.825004, 124.973758];
 
-let map = L.map("map").setView(coordsDolina, 10);
+let map = L.map("map").setView(coordsDefault, 14);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 25,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-// const gLayer = L.geoJSON(geoData[0]["features"]);
-// gLayer.setStyle(styles);
+function blackStyle(feature) {
+  return {
+    color: "black",
+    weight: 3,
+  };
+}
 
-// function styles(feature) {
-//   return {
-//     color: "black",
-//   };
-// }
+function hoverStyle(feature) {
+  return {
+    color: "#5e60e4",
+    weight: 5,
+  };
+}
+
+function hoverHandler(event) {
+  let layer = event.target;
+
+  layer.setStyle(hoverStyle());
+  layer.bringToFront();
+
+  info.update(layer.feature.properties);
+}
+
+function mouseOutHandler(event) {
+  balireNorthLayer.setStyle(blackStyle);
+  info.update();
+}
+
+function onEach(feature, layer) {
+  layer.on({
+    mouseover: hoverHandler,
+    mouseout: mouseOutHandler,
+  });
+}
+const balireNorthLayer = L.geoJSON(geoData["balireNorth"], {
+  onEachFeature: onEach,
+  style: blackStyle,
+});
+
+// Info box
+var info = L.control();
+
+info.onAdd = function (map) {
+  this.divAreaInfo = L.DomUtil.create("div", "info"); // create a div with a class "info"
+  this.update();
+  return this.divAreaInfo;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (properties) {
+  let innerHtml;
+  // let innerHtml = properties["Sys_Name"];
+  if (properties) {
+    innerHtml =
+      `<b>Region ${properties["Region"]}, ${properties["Pro_Name"]}</b><br>` +
+      `${properties["Sys_Name"]}<br>` +
+      `Area: ${properties["area"]} hectares (1 hectare = 10000m<sup>2</sup>)`;
+  } else {
+    innerHtml = "Hover over an area";
+  }
+
+  this.divAreaInfo.innerHTML = innerHtml;
+};
+
+info.addTo(map);
+
+balireNorthLayer.addTo(map);
 
 // function onEachFeatures(feature, layer) {
 //   // does this feature have a property named popupContent?
